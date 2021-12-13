ac0a09f99b10ff2e66769480a892f8ff2f31632f
theuncleofAlex <alex@yang2k.com>
2021-12-13 15:39:50 -0500

    Beat bar initial setup
    Prefab creation
    Boilerplate scripts

---
+++ Assets/Scripts/Graphics and UI/BeatBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class BeatBar : MonoBehaviour {
+    public GameObject BeatLine;
+
+    private List<BeatLine> _beatLines;
+    
+    void Start() {
+        _beatLines = new List<BeatLine>();
+        for (int i = 0; i < 10; ++i) {
+            BeatLine b = Instantiate(BeatLine).GetComponent<BeatLine>();
+            b.gameObject.SetActive(false);
+            _beatLines.Add(b);
+        }
+    }
+
+    void Update() {
+        // print(Conductor.Instance.currentClip.BPM);
+    }
+}
