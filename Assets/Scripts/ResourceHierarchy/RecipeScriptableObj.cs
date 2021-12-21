7f129b68663f63dcef443d0752c9e39b2339fbe3
theuncleofAlex <alex@yang2k.com>
2021-12-20 21:48:48 -0800

    Initial commit

---
+++ Assets/Scripts/ResourceHierarchy/RecipeScriptableObj.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Recipe", menuName = "ScriptableObjects/Recipe")]
+public class RecipeScriptableObj : ScriptableObject {
+    public ResourceNum[] InResources;
+    public ResourceNum[] OutResources;
+
+    public int ticks;
+    public bool CreatesNewOutput;
+}
