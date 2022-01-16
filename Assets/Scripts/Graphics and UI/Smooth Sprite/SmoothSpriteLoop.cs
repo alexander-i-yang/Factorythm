8e9aa1ccafe34e1e186936ccb32b72e4f0080987
theuncleofAlex <alex@yang2k.com>
2022-01-16 15:07:29 -0500

    Smooth Sprite Loop

---
+++ Assets/Scripts/Graphics and UI/Smooth Sprite/SmoothSpriteLoop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public enum positionState {
+    START,
+    END,
+}
+
+public class SmoothSpriteLoop : SmoothSprite {
+    public Vector3 endPos;
+    public Vector3 startPos;
+
+    [NonSerialized] public positionState pos = positionState.START;
+
+    void Start() {
+        startPos = transform.position;
+    }
+
+    public void Move() {
+        if (pos == positionState.START) {
+            base.Move(endPos);
+            pos = positionState.END;
+        } else if (pos == positionState.END) {
+            base.Move(startPos);
+            pos = positionState.START;
+        }
+    }
+}
---
+++ Assets/Scripts/Graphics and UI/Smooth Sprite/SmoothSpritesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothSpritesContoller : MonoBehaviour {
+    public SmoothSpriteLoop[] smoothSprites;
+
+    void Start() {
+        smoothSprites = GetComponentsInChildren<SmoothSpriteLoop>();
+    }
+
+    void Move() {
+        foreach (var s in smoothSprites) {
+            s.Move();
+        }
+    }
+}
