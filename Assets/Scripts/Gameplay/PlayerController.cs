8efdae1f858abb8d9d30da02f849750f892cf884
Ryan <ryanabusa8134@gmail.com>
2022-02-06 16:28:28 -0500

    Added Input System

--- Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,89 +1,73 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     private Rigidbody2D _myRb;
     private BoxCollider2D _myCollider;
     private SmoothSprite _mySR;
 
     private Room _curRoom;
-    
-    private bool _wasHoldingZ;
+
+    private bool _isHoldingZ;
+    private bool _isHoldingX;
     private InteractableStateMachine _ism;
+    private PlayerInputActions _pia;
 
     public bool RhythmLocked = true;
 
-    void Start() {
+
+    void Start()
+    {
         _myRb = GetComponent<Rigidbody2D>();
         _myCollider = GetComponent<BoxCollider2D>();
         _mySR = GetComponentInChildren<SmoothSprite>();
         _ism = GetComponent<InteractableStateMachine>();
+        _pia = new PlayerInputActions();
+
+        _pia.Player.Enable();
+        _pia.Player.Interact.performed += Interact;
+        _pia.Player.Interact.canceled += Interact;
+        _pia.Player.Delete.canceled += Delete;
+        _pia.Player.Delete.canceled += Delete;
+        _pia.Player.Movement.performed += Movement;
     }
-    
-    void Update() {
-        int inputH = checkInputH();
-        int inputV = checkInputV();
-        Vector3 newPos = Vector3.zero;
-        bool moved = false;
-        bool attemptMove = inputH != 0 || inputV != 0;
 
+    void Update()
+    {
+        _ism.SetZPressed(_isHoldingZ);
+        _ism.SetXPressed(_isHoldingX);
 
-        if (attemptMove) {
-            bool onBeat;
-            if (!Conductor.Instance.RhythmLock) {
-                onBeat = true;
-                // Conductor.Instance.MachineTick();
-            } else {
-                if (this.RhythmLocked) {
-                    onBeat = Conductor.Instance.AttemptMove();
-                } else {
-                    onBeat = true;
-                }
-            }
-
-            if (onBeat) {
-                if (inputH != 0) {
-                    newPos = _myRb.position + new Vector2(inputH, 0);
-                }
-                
-                if (inputV != 0) {
-                    newPos = _myRb.position + new Vector2(0, inputV);
-                }
-
-                _mySR.Move(newPos);
-                _myRb.MovePosition(newPos);
-                _ism.Move(newPos);
-                moved = true;
-            }
-        }
-
-        bool curZ = Input.GetKey(KeyCode.Z);
-        _ism.SetZPressed(curZ);
-        bool curX = Input.GetKey(KeyCode.X);
-        _ism.SetXPressed(curX);
-        
         // print("Cur: " + _ism.CurInput.CurInteractable);
         // print("Z press: " + curZ);
         // print("On top of: " + (OnInteractable(newPos) != null));
 
-        _wasHoldingZ = curZ;
-
-        if (Conductor.Instance.SongIsOnBeat()) {
+        if (Conductor.Instance.SongIsOnBeat())
+        {
             _mySR.GetComponent<SpriteRenderer>().color = Color.red;
-        } else {
+        }
+        else
+        {
             _mySR.GetComponent<SpriteRenderer>().color = Color.blue;
         }
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         Collider2D roomCollider = CheckRoomOverlap();
-        if (roomCollider) {
-            if (!_curRoom) {
+        if (roomCollider)
+        {
+            if (!_curRoom)
+            {
                 _curRoom = roomCollider.GetComponent<Room>();
                 _curRoom.OnPlayerEnter(this);
             }
-        } else {
-            if (_curRoom) {
+        }
+        else
+        {
+            if (_curRoom)
+            {
                 _curRoom.OnPlayerExit(this);
             }
 
@@ -91,32 +75,38 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    private Collider2D CheckRoomOverlap() {
+    private Collider2D CheckRoomOverlap()
+    {
         Vector3 alpha = new Vector3(0.05f, 0.05f); //So the player touching the edge of the collider isn't counted as an overlap
         Vector2 topLeftCorner = _myCollider.bounds.min + alpha;
         Vector2 topRightCorner = _myCollider.bounds.max - alpha;
         Collider2D overlapCollider = Physics2D.OverlapArea(
-            topLeftCorner, 
+            topLeftCorner,
             topRightCorner,
             LayerMask.GetMask("Room")
         );
         return overlapCollider;
     }
-    
-    public Interactable OnInteractable(Vector3 pos) {
+
+    public Interactable OnInteractable(Vector3 pos)
+    {
         RaycastHit2D hit = Physics2D.Raycast(
             pos,
             new Vector3(0, 0, 1),
-            10.0f, 
+            10.0f,
             LayerMask.GetMask("Interactable"));
-        if (hit.transform != null) {
+        if (hit.transform != null)
+        {
             return hit.transform.GetComponent<Interactable>();
-        } else {
+        }
+        else
+        {
             return null;
         }
     }
 
-    public Interactable OnInteractable() {
+    public Interactable OnInteractable()
+    {
         return OnInteractable(transform.position);
     }
 
@@ -131,7 +121,8 @@ public class PlayerController : MonoBehaviour {
         {
             return hit.transform.gameObject.GetComponent<Destructable>();
         }
-        else {
+        else
+        {
             return null;
         }
     }
@@ -141,13 +132,67 @@ public class PlayerController : MonoBehaviour {
         return OnDestructable(transform.position);
     }
 
-    int checkInputH() {
+    #region Actions
+    private void Movement(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = _pia.Player.Movement.ReadValue<Vector2>();
+        Vector3 newPos = Vector3.zero;
+        // bool moved = false;
+        // bool attemptMove = inputVector != Vector2.zero;
+
+        bool onBeat;
+
+        if (!Conductor.Instance.RhythmLock)
+        {
+            onBeat = true;
+            // Conductor.Instance.MachineTick();
+        }
+        else
+        {
+            if (this.RhythmLocked)
+            {
+                onBeat = Conductor.Instance.AttemptMove();
+            }
+            else
+            {
+                onBeat = true;
+            }
+        }
+
+        if (onBeat)
+        {
+            newPos.x = _myRb.position.x
+            + (inputVector.x > 0 ? 1 : (inputVector.x < 0 ? -1 : 0));
+            newPos.y = _myRb.position.y
+            + (inputVector.y > 0 ? 1 : (inputVector.y < 0 ? -1 : 0));
+
+            _mySR.Move(newPos);
+            _myRb.MovePosition(newPos);
+            _ism.Move(newPos);
+        }
+    }
+
+    private void Interact(InputAction.CallbackContext context)
+    {
+        _isHoldingZ = context.performed;
+    }
+
+    private void Delete(InputAction.CallbackContext context)
+    {
+        _isHoldingX = context.performed;
+    }
+
+    #endregion
+
+    int checkInputH()
+    {
         bool leftPress = Input.GetKeyDown("left");
         bool rightPress = Input.GetKeyDown("right");
         return leftPress ? -1 : (rightPress ? 1 : 0);
     }
-    
-    int checkInputV() {
+
+    int checkInputV()
+    {
         bool upPress = Input.GetKeyDown("up");
         bool downPress = Input.GetKeyDown("down");
         return upPress ? 1 : (downPress ? -1 : 0);
