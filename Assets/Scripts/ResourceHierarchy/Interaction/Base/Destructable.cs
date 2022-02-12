95f596145e694ea7d6198ce403c23cf19aff0b4d
Afsynces <tziegler30@gatech.edu>
2022-02-12 14:15:27 -0500

    Remembered to comment my code

--- Assets/Scripts/ResourceHierarchy/Interaction/Base/Destructable.cs
+++ Assets/Scripts/ResourceHierarchy/Interaction/Base/Destructable.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//If you want to make a new destructable method, make a new C# script and put Destructable as the base class
+//Then just put it on your object and you're good to go
 public abstract class Destructable : MonoBehaviour
 {
-    //Called when the object is destroyed, but OnDestroy is a unity method, so it is called this stupid thing
+    //Called when the object is destroyed
     public abstract void OnDestruct();
 }
--- Assets/Scripts/ResourceHierarchy/Interaction/Base/NormalDestroy.cs
+++ Assets/Scripts/ResourceHierarchy/Interaction/Base/NormalDestroy.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Handles normal destructions of objects
 public class NormalDestroy : Destructable
 {
+    //Called when the object is destroyed
     public override void OnDestruct()
     {
         gameObject.SetActive(false);
