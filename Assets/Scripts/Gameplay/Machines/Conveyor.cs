d3ab2951af8994d49ed734b46e47cab0f16ada8d
theuncleofAlex <alex@yang2k.com>
2022-03-08 18:30:01 -0500

    Fixed Diagonal Conveyor issue

--- Assets/Scripts/Gameplay/Machines/Conveyor.cs
+++ Assets/Scripts/Gameplay/Machines/Conveyor.cs
@@ -90,7 +90,7 @@ public class Conveyor : Machine {
         // ResetSprite();
     }
     
-    #if UNITY_EDITOR
+    /*#if UNITY_EDITOR
     public void OnDrawGizmos() {
         base.OnDrawGizmos();
         Handles.Label(transform.position + new Vector3(0.1f, 0.1f, -3), "I " + _inputLoc);
@@ -98,5 +98,5 @@ public class Conveyor : Machine {
         Handles.Label(transform.position + new Vector3(0.1f, 0.5f, -3), "S " + _spriteIndex + " A " + Angle);
         Handles.Label(transform.position + new Vector3(0.1f, 0.7f, -3), "BH" + _betweenMachines + " B" + BetweenMachines);  
     }
-    #endif
+    #endif*/
 }
