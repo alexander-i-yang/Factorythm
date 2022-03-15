d294cdf6f4f32385db8533cd451d31a4104b25cd
theuncleofAlex <alex@yang2k.com>
2022-03-15 02:12:04 -0400

    Combined into window

--- Assets/Scripts/Helper/EditorHelper.cs
+++ Assets/Scripts/Helper/EditorHelper.cs
@@ -3,33 +3,57 @@ using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
-public class EditorHelper : MonoBehaviour {
-    [MenuItem("EditorHelper/SliceSprites")]
+public class EditorHelper : EditorWindow {
+    public static int sliceWidth = 32;
+    public static int sliceHeight = 32;
+
+    public static string importPath = "Assets/Sprites/Conveyors";
+
+    public static string exportPath = "Assets/Animation/Conveyors/";
+
+    [MenuItem("Window/Sprite Animator")]
+    static void Init() {
+        // Window Set-Up
+        EditorHelper window =
+            EditorWindow.GetWindow(typeof(EditorHelper), false, "AnimationGenerator", true) as
+                EditorHelper;
+        window.minSize = new Vector2(200, 200);
+        window.maxSize = new Vector2(400, 400);
+        window.Show();
+    }
+
+    //Show UI
+    void OnGUI() {
+        sliceWidth = EditorGUILayout.IntField("Slice Width", sliceWidth);
+        sliceHeight = EditorGUILayout.IntField("Slice Width", sliceHeight);
+        importPath = EditorGUILayout.TextField("Import Path", importPath);
+        exportPath = EditorGUILayout.TextField("Export Path", exportPath);
+
+        if (GUILayout.Button("Generate Animation")) {
+            SliceSprites();
+        }
+        else {
+            Debug.LogWarning("Forgot to set the textures?");
+        }
+
+        Repaint();
+    }
+
     static void SliceSprites() {
-        // Change the below for the with and height dimensions of each sprite within the spritesheets
-        int sliceWidth = 32;
-        int sliceHeight = 32;
+        // Find all Texture2Ds that have 'co' in their filename, that are labelled with 'architecture' and are placed in 'MyAwesomeProps' folder
+        string[] guids = AssetDatabase.FindAssets("t:texture2D", new[] {importPath});
+        Debug.Log("spriteSheets.Length: " + guids.Length);
 
-        // Change the below for the path to the folder containing the sprite sheets (warning: not tested on folders containing anything other than just spritesheets!)
-        // Ensure the folder is within 'Assets/Resources/' (the below example folder's full path within the project is 'Assets/Resources/ToSlice')
-        string folderPath = "Conveyors";
-
-        Object[] spriteSheets = Resources.LoadAll(folderPath, typeof(Texture2D));
-        Debug.Log("spriteSheets.Length: " + spriteSheets.Length);
-
-        for (int z = 0; z < spriteSheets.Length; z++) {
-            Debug.Log("z: " + z + " spriteSheets[z]: " + spriteSheets[z]);
-
-            string path = AssetDatabase.GetAssetPath(spriteSheets[z]);
+        for (int z = 0; z < guids.Length; z++) {
+            string assetPath = AssetDatabase.GUIDToAssetPath (guids[z]);
+            Texture2D spriteSheet = AssetDatabase.LoadAssetAtPath (assetPath, typeof(Texture2D)) as Texture2D;
+            string path = AssetDatabase.GetAssetPath(spriteSheet);
             TextureImporter ti = AssetImporter.GetAtPath(path) as TextureImporter;
             ti.isReadable = true;
             ti.spriteImportMode = SpriteImportMode.Multiple;
 
             List<SpriteMetaData> newData = new List<SpriteMetaData>();
-
-            Texture2D spriteSheet = spriteSheets[z] as Texture2D;
 
             for (int i = 0; i < spriteSheet.width; i += sliceWidth) {
                 for (int j = spriteSheet.height; j > 0; j -= sliceHeight) {
@@ -45,65 +69,21 @@ public class EditorHelper : MonoBehaviour {
 
             ti.spritesheet = newData.ToArray();
             AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+            Sprite[] sprites = CutSprites(spriteSheet);
+            makeAnimation(spriteSheet.name, sprites);
         }
 
         Debug.Log("Done Slicing!");
     }
-}
 
-public class AutoAnimationCreator : EditorWindow {
-    public int frameHeight;
-    public int frameWidth;
-    public Texture2D pasteTo; //Sprite atlas where to paste settings
-    public Texture2D[] SpriteSheets;
-    [MenuItem("Window/Sprite Animator")]
-    static void Init() {
-        // Window Set-Up
-        AutoAnimationCreator window =
-            EditorWindow.GetWindow(typeof(AutoAnimationCreator), false, "AnimationGenerator", true) as
-                AutoAnimationCreator;
-        window.minSize = new Vector2(260, 170);
-        window.maxSize = new Vector2(260, 170);
-        window.Show();
-    }
-
-    //Show UI
-    void OnGUI() {
-        // "target" can be any class derived from ScriptableObject 
-        // (could be EditorWindow, MonoBehaviour, etc)
-        ScriptableObject target = this;
-        SerializedObject so = new SerializedObject(target);
-        SerializedProperty stringsProperty = so.FindProperty("SpriteSheets");
-
-        EditorGUILayout.PropertyField(stringsProperty, true); // True means show children
-        so.ApplyModifiedProperties(); // Remember to apply modified properties
-        // _spriteSheets = (Texture2D[]) EditorGUILayout.ObjectField("SpriteSheets", _spriteSheets, typeof(Texture2D[]), true);
-
-        EditorGUILayout.Space();
-
-        if (GUILayout.Button("Generate Animation")) {
-            foreach (var spriteSheet in SpriteSheets) {
-                if (spriteSheet != null) {
-                    Sprite[] sprites = cutSprites(spriteSheet);
-                    makeAnimation(spriteSheet.name, sprites);
-                }
-            }
-        }
-        else {
-            Debug.LogWarning("Forgot to set the textures?");
-        }
-
-        Repaint();
-    }
-
-    private Sprite[] cutSprites(Texture2D spriteSheet) {
+    private static Sprite[] CutSprites(Texture2D spriteSheet) {
         if (!IsAtlas(spriteSheet)) {
             Debug.LogWarning("Unable to proceed, the source texture is not a sprite atlas.");
             return null;
         }
 
         Sprite[] sprites = null;
-        
+
         //Proceed to read all sprites from CopyFrom texture and reassign to a TextureImporter for the end result
         UnityEngine.Object[] _objects =
             AssetDatabase.LoadAllAssetRepresentationsAtPath(AssetDatabase.GetAssetPath(spriteSheet));
@@ -118,7 +98,7 @@ public class AutoAnimationCreator : EditorWindow {
         return sprites;
     }
 
-    private void makeAnimation(String name, Sprite[] sprites) {
+    private static void makeAnimation(String name, Sprite[] sprites) {
         //http://forum.unity3d.com/threads/lack-of-scripting-functionality-for-creating-2d-animation-clips-by-code.212615/
         AnimationClip animClip = new AnimationClip();
         // First you need to create e Editor Curve Binding
@@ -143,11 +123,11 @@ public class AutoAnimationCreator : EditorWindow {
         }
 
         AnimationUtility.SetObjectReferenceCurve(animClip, curveBinding, keyFrames);
-        AssetDatabase.CreateAsset(animClip, "Assets/Animation/Conveyors/" + name + ".anim");
+        AssetDatabase.CreateAsset(animClip, exportPath + name + ".anim");
     }
 
     //Check that the texture is an actual atlas and not a normal texture
-    private bool IsAtlas(Texture2D tex) {
+    private static bool IsAtlas(Texture2D tex) {
         string _path = AssetDatabase.GetAssetPath(tex);
         TextureImporter _importer = AssetImporter.GetAtPath(_path) as TextureImporter;
 
@@ -155,6 +135,4 @@ public class AutoAnimationCreator : EditorWindow {
                _importer.spriteImportMode == SpriteImportMode.Multiple;
     }
 }
-
-
 #endif
