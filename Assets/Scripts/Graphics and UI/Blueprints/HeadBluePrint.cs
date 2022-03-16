4e22b9e15b49a87fa8bca06c7ebc8105fdab00a9
Christopher Kim <75755153+ChrisAKim@users.noreply.github.com>
2022-03-15 21:08:07 -0400

    Blueprint turns green/red where it can/can't be placed
    Moved the CheckTileOn() method to MachineBluePrint.cs from PlayerController.cs and removed boolean flags. CanPlace(pos) calls CheckTileOn using pos as the raycast origin.

--- Assets/Scripts/Graphics and UI/Blueprints/HeadBluePrint.cs
+++ Assets/Scripts/Graphics and UI/Blueprints/HeadBluePrint.cs
@@ -7,7 +7,11 @@ using UnityEngine;
 public class HeadBluePrint : MachineBluePrint
 {
     public override bool CanPlace(Vector3 pos) {
-        PlayerController p = (PlayerController) (GameObject.Find("Player").GetComponent(typeof(PlayerController)));
-        return p.CanPlaceHeadMine;
+        RaycastHit2D hit = this.CheckTileOn(pos);
+        if (hit.transform == null) {
+            return false;
+        } else {
+            return hit.transform.gameObject.CompareTag("HeadTiles");
+        }
     }
 }
