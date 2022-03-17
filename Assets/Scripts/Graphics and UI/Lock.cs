9edcfabf091320efdd0e32d124fd0a75228459c2
theuncleofAlex <alex@yang2k.com>
2022-03-17 03:45:22 -0400

    Added Jason's Sprite

---
+++ Assets/Scripts/Graphics and UI/Lock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Lock : MonoBehaviour {
+    private Animator _myAnimator;
+
+    void Awake() {
+        _myAnimator = GetComponent<Animator>();
+    }
+
+    public void Unlock() {
+        _myAnimator.SetTrigger("Unlock");
+    }
+}
